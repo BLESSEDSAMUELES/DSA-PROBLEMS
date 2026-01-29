86f9b47c642f12c817d9094011e09ae223a01828
BLESSEDSAMUELES <71379210+BLESSEDSAMUELES@users.noreply.github.com>
2026-01-29 22:44:25 +0530

    Time: 2102 ms (31.24%) | Memory: 42 MB (74.40%) - LeetSync

---
+++ 507-perfect-number/perfect-number.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean checkPerfectNumber(int num) {
+        int s=0;
+        for(int i=1;i<(num/2)+1;i++){
+            if(num%i==0){
+                s+=i;
+            }
+        }
+        if(s==num)
+            return true;
+        return false;
+    }
+}
