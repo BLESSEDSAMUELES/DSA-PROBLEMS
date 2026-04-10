b45be544e4d0c6bb76eeeadd869004b9c1cd036b
Blessed Samueles N G <71379210+BLESSEDSAMUELES@users.noreply.github.com>
2026-04-10 22:04:03 +0530

    Time: 6 ms (82.18%) | Memory: 44.5 MB (53.55%) - LeetSync

---
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String reverseWords(String s) {
+        StringBuilder sb=new StringBuilder();
+        String a=s.trim();
+        String words[] = a.split(" +");
+        for(int i=words.length-1;i>=0;i--){
+            sb.append(words[i]);
+            if(i!=0){
+                sb.append(" ");
+            }
+        }
+        return sb.toString();
+    }
+}
